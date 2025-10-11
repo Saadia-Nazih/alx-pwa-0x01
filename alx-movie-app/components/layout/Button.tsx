ee9115bc39cee29278fb871281e7f13b3108891a
Saadia Nazih <saadia.nazih92@gmail.com>
2025-10-11 23:11:34 +0200

    new update

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Definiamo le props che il nostro pulsante può ricevere
+interface ButtonProps {
+  title: string;
+  action?: () => void; // Nota: la prop 'action' è ora facoltativa grazie al '?'
+}
+
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  return (
+    <button
+      onClick={action}
+      className="bg-[#E2D609] text-black font-bold py-3 px-8 rounded-lg hover:bg-yellow-400 transition duration-300"
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
